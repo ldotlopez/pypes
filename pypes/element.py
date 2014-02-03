437aa1e9ced0277d85c4d9885d377d0138661aa4
Luis Lopez <luis@cuarentaydos.com>
2014-02-03 17:39:27 +0100

    Working pipeline

--- pypes/element.py
+++ pypes/element.py
@@ -1,119 +1,63 @@
 from functools import wraps
-from .queue import Queue, WritePad, ReadPad, \
-        Empty, EOF, ReadError, WriteError
+from .logging import get_logger
 
-class Done(Exception): pass
-class NoInput(Exception): pass
-class NoOutput(Exception): pass
+class WriteError(Exception): pass
+class ReadError(Exception): pass
+class UnknowElement(Exception): pass
+class Finish(Exception): pass
+class Empty(Exception): pass
+class EOF(Exception): pass
 
-def check_alive(f):
-    @wraps(f)
-    def wrapped(inst, *args, **kwargs):
-        if not inst.alive:
-            raise Done()
-        return f(inst, *args, **kwargs)
-
-    return wrapped
-
-def check_input(f):
-    @wraps(f)
-    def wrapped(inst, *args, **kwargs):
-        if not inst.input:
-            raise NoInput()
-        return f(inst, *args, **kwargs)
-
-    return wrapped
-
-def check_output(f):
-    @wraps(f)
-    def wrapped(inst, *args, **kwargs):
-        if not inst.output:
-            raise NoOutput()
-        return f(inst, *args, **kwargs)
-
-    return wrapped
-
+_logger = get_logger()
 
 class Element:
-    def __init__(self):
-        self._alive = True
-        self._input, self._output = None, None
-
-    def attach(self, container, input=None, output=None):
+    def attach(self, container):
         self._container = container
-        self._input, self._output = input, output
 
     def __str__(self):
         return "{}-{}".format(self.__class__.__name__, id(self))
 
-    @property
-    def alive(self):
-        return any([x is not None for x in [self._input, self._output]])
 
-    @property
-    def input(self):
-        return self._input
-
-    @property
-    def output(self):
-        return self._output
-
-    @input.setter
-    def input(self, queue):
-        if self._input:
-            raise ValueError()
-        self._input = queue
-
-    @output.setter
-    def output(self, queue):
-        if self._output:
-            raise ValueError()
-        self._output = queue
-
-    @check_input
-    @check_alive
     def get(self):
-        return self.input.get()
+        packet = self._container.get(self)
+        _logger.debug("GET [{}] <- {}".format(self, packet))
+        return packet
 
-    @check_output
-    @check_alive
-    def put(self, data):
-        self.output.put(data)
+    def put(self, packet):
+        _logger.debug("PUT [{}] -> {}".format(self, packet))
+        self._container.put(self, packet)
 
     def finish(self):
-        self._input, self._output = None, None
-        self._alive = False
-        raise Done()
+        _logger.debug("FINISH {}".format(self))
+        raise Finish()
+
 
 class SampleSrc(Element):
     def __init__(self, sample=('foo', 'bar', 'frob')):
         super(SampleSrc, self).__init__()
         self._sample = list(sample)
 
-    @check_alive
     def run(self):
         try:
             self.put(self._sample.pop(0))
             return True
 
         except IndexError:
-            self.output.close()
             self.finish()
-            
+
 
 class NullSink(Element):
     def run(self):
         try:
             x = self.get()
+            return True
 
         except Empty:
             return False
 
-        except Eof:
-            self.input.close()
+        except EOF:
             self.finish()
 
-        return True
 
 class StoreSink(Element):
     def __init__(self):
@@ -131,36 +75,131 @@ class StoreSink(Element):
         except EOF:
             self.finish()
 
+
 class Pipeline:
     def __init__(self):
         self._elements = set()
 
+        # Dict of queues using src element has key
+        self._queues = {}
+
+        # Dict of queues using sink element has key
+        self._rev_queues = {} 
+
+        # Relations between srcs and sinks
+        self._rels = {}
+
+        # Relations between sinks and srcs
+        self._rev_rels = {}
+
     def connect(self, src, sink):
         # Insert elements
         self._elements.add(src)
+        src.attach(self)
+
         self._elements.add(sink)
+        sink.attach(self)
 
-        # Connect both elements thru a queue and pads
-        q = Queue()
-        src.output = WritePad(q)
-        sink.input = ReadPad(q)
+        q = []
+        self._queues[src] = q
+        self._rev_queues[sink] = q
 
-        print("Connect {} -> {}".format(str(src), str(sink)))
+        self._rels[src] = sink
+        self._rev_rels[sink] = src
+
+        _logger.debug("CONNECT [{}] -> [{}]".format(str(src), str(sink)))
+
+    def is_src(self, src):
+        return src in self._queues
+
+    def is_sink(self, sink):
+        return sink in self._rev_queues
+
+    def src_for(self, sink):
+        try:
+            return self._rev_rels[sink]
+        except KeyError:
+            raise UnknowElement()
+
+    def sink_for(self, src):
+        try:
+            return self._rels.get[src]
+        except KeyError:
+            raise UnknowElement()
+
+    def write_queue(self, src):
+        if not self.is_src(src):
+            raise WriteError()
+
+        return self._queues[src]
+
+    def read_queue(self, sink):
+        if not self.is_sink(sink):
+            raise ReadError()
+
+        # src_for_sink = self.src_for(sink)
+        return self._rev_queues[sink]
+
+
+    def put(self, element, packet):
+        """Puts a packet from elment into the pipeline flow
+        Raises UnknowElement if element is not in pipeline
+        Raises WriteError if element has no writeable queue
+        """
+
+        if not element in self._elements:
+            raise UnknowElement()
+
+        self.write_queue(element).append(packet)
+
+
+    def get(self, sink):
+        """Gets a packet for sink from the pipeline flow
+        Raises Empty if there is not data to read
+        Raises UnknowElement if sink is not in pipeline
+        Raises ReadError if sink has no readable queue
+        Raises EOF if there is no more data and not write sink on the other side
+        """
+        
+        if not sink in self._elements:
+            raise UnknowElement()
+
+        if not self.is_sink(sink):
+            raise ReadError()
+
+        try:
+            return self.read_queue(sink).pop(0)
+        except IndexError:
+            # Check if queue has reached EOF
+            src = self.src_for(sink)
+            if src not in self._queues:
+                raise EOF()
+            else:
+                raise Empty()
+
+    def disconnect(self, element):
+        if self.is_src(element):
+            del self._rels[element]
+            del self._queues[element]
+
+        if self.is_sink(element):
+            del self._rev_queues[element]
+            del self._rev_rels[element]
+
+
+        self._elements.remove(element)
 
     def run(self):
-        if not self._elements:
-            return False
+        finished = []
 
-        dones = []
-        for e in self._elements:
-            print("Run {}".format(e))
+        for element in self._elements:
             try:
-                e.run()
-            except Done:
-                dones.append(e)
-                print(" - raise done")
+                element.run()
+            except Finish:
+                finished.append(element)
 
-        for e in dones:
-            self._elements.remove(e)
+        for element in finished:
+            self.disconnect(element)
 
-        return True
+        return len(self._elements) > 0
+
