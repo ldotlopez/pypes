9c6cd56821aca7e47624e182cb9ea7c3f1af8513
Luis López <ldotlopez@gmail.com>
2014-03-17 18:26:55 +0100

    Packaging files

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='pypes',
+    version='0.0.0.20140317',
+    author='L. López',
+    author_email='ldotlopez@gmail.com',
+    packages=['pypes', 'pypes.tests'],
+    scripts=[],
+    url='https://github.com/ldotlopez/pypes',
+    license='LICENSE.txt',
+    description='Flow-based programming for Python 3',
+    long_description=open('README').read(),
+    install_requires=[
+    ],
+)
